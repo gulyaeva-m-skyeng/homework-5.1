934cd0fc9cb95ad6e8012280ddc5d525af5896f4
gulyaeva-m-skyeng <marucosmos0924@gmail.com>
2024-11-30 18:49:08 +0300

    задание 2

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,46 @@ public class Main {
     public static void main(String[] args) {
         //task 1
         System.out.println("Task 1");
-        int[] numbers = new int[] {1, 2, 3};
+        int[] numbers = new int[]{1, 2, 3};
         double[] decimals = {1.57, 7.654, 9.986};
 
-                String[] employeeNames = {"Kate", "Olya", "Lena"};
-                int[] employeeAges = {31, 40, 27};
-                double[] employeeSalaries = new double[3];
-                employeeSalaries[0] = 3777.71;
-                employeeSalaries[1] = 4505.57;
-                employeeSalaries[2] = 7077.75;
+        String[] employeeNames = {"Kate", "Olya", "Lena"};
+        int[] employeeAges = {31, 40, 27};
+        double[] employeeSalaries = new double[3];
+        employeeSalaries[0] = 3777.71;
+        employeeSalaries[1] = 4505.57;
+        employeeSalaries[2] = 7077.75;
+
+
+        //task 2
+        System.out.println("Task 2");
+        int[] arr1 = new int[]{4, 5, 6};
+        double[] arr2 = new double[]{5.5, 6.65, 7.896};
+        String[] arr3 = new String[]{"mango", "orange", "cacos"};
+
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.print(arr1[i]);
+            if (i != arr1.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i]);
+            if (i != arr2.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        for (int i = 0; i < arr3.length; i++) {
+            System.out.print(arr3[i]);
+            if (i != arr3.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
     }
 }
+
