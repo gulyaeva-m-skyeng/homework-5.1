8715b152723e8401b0b4aca7ac8ab09c724537a4
gulyaeva-m-skyeng <marucosmos0924@gmail.com>
2024-11-30 18:09:38 +0300

    задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //task 1
+        System.out.println("Task 1");
+        int[] numbers = new int[] {1, 2, 3};
+        double[] decimals = {1.57, 7.654, 9.986};
+
+                String[] employeeNames = {"Kate", "Olya", "Lena"};
+                int[] employeeAges = {31, 40, 27};
+                double[] employeeSalaries = new double[3];
+                employeeSalaries[0] = 3777.71;
+                employeeSalaries[1] = 4505.57;
+                employeeSalaries[2] = 7077.75;
     }
 }
